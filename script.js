8cf46b200b2c9ae889edb31f16b92ee48bfca367
motasamokour407-ctrl <motasamokour407@gmail.com>
2025-12-31 18:31:22 +0300

    Add startSurprise function for New Year alert

---
+++ script.js
@@ -0,0 +1,5 @@
+function startSurprise() {
+  alert("🎉 أحبك بكل لحظة من السنة الجديدة!");
+  document.body.style.background = "black";
+  document.getElementById("fireworks").innerHTML = "✨🎆✨🎆✨";
+}
